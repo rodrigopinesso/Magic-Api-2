98aa469c474491ca6766ba4a608504f75bcc82fc
Rodrigo Pinesso <142932782+rodrigopinesso@users.noreply.github.com>
2024-09-28 06:53:09 -0300

    Delete dist directory

--- dist/deck-creator/deck-creator.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { DeckCreatorService } from './deck-creator.service';
-import { Deck } from '../deck/schemas/deck.schema';
-export declare class DeckCreatorController {
-    private readonly deckCreatorService;
-    constructor(deckCreatorService: DeckCreatorService);
-    createDeck(commanderName: string, deckName: string): Promise<Deck>;
-}

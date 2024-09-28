41dc712f33cee20907e9cc8dfbcc1078f5699f9b
Rodrigo Pinesso <142932782+rodrigopinesso@users.noreply.github.com>
2024-09-28 16:53:47 -0300

    Delete dist directory

--- dist/deck/dto/update-deck-dto.d.ts
+++
@@ -1,7 +0,0 @@
-import { Colors } from "../schemas/deck.schema";
-export declare class updateDeckDto {
-    readonly name: string;
-    readonly commanderName: string;
-    readonly cards: string[];
-    readonly colors: Colors[];
-}

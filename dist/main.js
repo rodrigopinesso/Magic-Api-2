c83ffe37a4bdf0666189263406bd7508ea5377c0
renza <jaorenzetti@outlook.com>
2024-09-28 21:10:45 -0300

    testes

--- dist/main.js
+++ dist/main.js
@@ -2,9 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const os_1 = require("os");
+const cluster = require('cluster');
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+    const isPrimary = cluster.isPrimary || cluster.isMaster;
+    if (isPrimary) {
+        const numCPUs = (0, os_1.cpus)().length;
+        console.log(`Master process started. Forking ${numCPUs} workers...`);
+        for (let i = 0; i < numCPUs; i++) {
+            cluster.fork();
+        }
+        cluster.on('exit', (worker, code, signal) => {
+            console.log(`Worker ${worker.process.pid} died. Forking a new worker...`);
+            cluster.fork();
+        });
+    }
+    else {
+        const app = await core_1.NestFactory.create(app_module_1.AppModule);
+        await app.listen(3000);
+    }
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { cpus } from 'os';
+const cluster = require('cluster');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  const isPrimary = cluster.isPrimary || cluster.isMaster;  
+  
+  if (isPrimary) {
+    const numCPUs = cpus().length;
+    console.log(`Master process started. Forking ${numCPUs} workers...`);
+    
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork(); 
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} died. Forking a new worker...`);
+      cluster.fork();
+    });
+  } else {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+  
+  }
 }
 bootstrap();

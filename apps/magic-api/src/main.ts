d7543f5cc36bb2d8366d111a05c520e2d58323fa
NathanSenaa <128058745+NathanSenaa@users.noreply.github.com>
2024-12-01 23:22:13 -0300

    comentado

--- apps/magic-api/src/main.ts
+++ apps/magic-api/src/main.ts
@@ -4,30 +4,36 @@ import { cpus } from 'os';
 const cluster = require('cluster');
 
 async function bootstrap() {
+  // Verifica se o processo atual é o primário (master) no cluster
   const isPrimary = cluster.isPrimary || cluster.isMaster;  
 
   if (isPrimary) {
-    const numCPUs = cpus().length;
+    const numCPUs = cpus().length; // Obtém o número total de CPUs disponíveis
     console.log(`Master process started. Forking ${numCPUs} workers...`);
     
+    // Cria um processo para cada CPU disponível
     for (let i = 0; i < numCPUs; i++) {
       cluster.fork(); 
     }
 
+    // Listener para reiniciar o worker caso ele falhe
     cluster.on('exit', (worker, code, signal) => {
       console.log(`Worker ${worker.process.pid} died. Forking a new worker...`);
       cluster.fork();
     });
   } else {
+    // Inicia o aplicativo NestJS em um processo de trabalho
     const app = await NestFactory.create(AppModule);
     
+    // Configurações de CORS específicas para permitir origens locais
     app.enableCors({
-      origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      credentials: true,
+      origin: ['http://127.0.0.1:5500', 'http://localhost:5500'], // Define origens permitidas
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Define métodos permitidos
+      credentials: true, // Permite envio de credenciais
     });
 
-    await app.listen(3000);//estava listn(3000)
+    // Configura a porta de escuta. Comentário ajustado para melhorar a clareza
+    await app.listen(3000); // Estava apenas "listen(3000)"
     console.log(`Worker process started on port 3000. PID: ${process.pid}`);
   }
 }
